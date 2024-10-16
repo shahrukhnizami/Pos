fa3fdade29d69509c9626af0a0ddfe43dde6856f
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-10-16 09:29:32 +0500

    ud

--- src/pages/Admin/Orders.jsx
+++ src/pages/Admin/Orders.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button, Form, Input, Spin } from "antd"; // Import Spin for loading indicator
 import { UserList } from "../../components/UserList";
-import { AddUserDrawer } from "../../components/AdduserDrawer";
+import { AddUserDrawer } from "../../components/AddUserDrawer";
 import { OrdersList } from "../../components/OrdesList";
 
 function Orders() {
