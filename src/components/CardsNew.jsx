18ba50f15cda1d85ad1f04dae03d3fef886c2dd7
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-10-16 09:37:03 +0500

    Cart

--- src/components/CardsNew.jsx
+++ src/components/CardsNew.jsx
@@ -1,7 +1,7 @@
 import { Button } from 'antd';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { CartContext } from '../Context/CartContext';
+import { CartContext } from '../context/CartContext';
 
 
 
