f50861399a9be7ace93d3e41b6be1e7c4ea52dad
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-10-23 03:20:04 +0500

    Update Users

--- src/pages/Admin/Users.jsx
+++ src/pages/Admin/Users.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import { Button, Form, Input, Spin } from "antd"; // Import Spin for loading indicator
+import { Button, Form, Input } from "antd"; 
 import { UserList } from "../../components/UserList";
 import { AddUserDrawer } from "../../components/AddUserDrawer";
 
 function Users() {
-  // const [loading, setLoading] = useState(false); // Loading state
   const [open, setOpen] = useState(false);
   const [query, setQuery] = useState(""); // State for search query
 
@@ -17,47 +16,36 @@ function Users() {
   };
 
   const handleAddUser = async (userData) => {
-    // setLoading(true); // Start loading
     try {
       // Add your user addition logic here
-      // e.g., await addUserToDB(userData);
     } catch (error) {
       console.error('Error adding user:', error);
     } finally {
-      // setLoading(false); // Stop loading
       onClose(); // Close the drawer after adding user
     }
   };
 
   return (
-    <div>
-      <div className="flex justify-between mb-5">
-        <h1 className="text text-5xl font-bold">All Users</h1>
-        <Button danger type="primary" onClick={showDrawer}>
+    <div className="container mx-auto px-4">
+      <div className="flex flex-col md:flex-row justify-between items-center mb-5">
+        <h1 className="text-2xl md:text-5xl font-bold">All Users</h1>
+        <Button danger type="primary" onClick={showDrawer} className="mt-4 md:mt-0">
           Add User
         </Button>
       </div>
+
       <Form>
-      <Form.Item label="Search" name="search" type="text">
-        <Input
-          placeholder="Search Users"
-          onChange={(e) => setQuery(e.target.value)} // Capture search query
-        />
-      </Form.Item>
+        <Form.Item label="Search" name="search" className="mb-4">
+          <Input
+            placeholder="Search Users"
+            onChange={(e) => setQuery(e.target.value)} // Capture search query
+          />
+        </Form.Item>
       </Form>
 
       <AddUserDrawer onClose={onClose} open={open} onAddUser={handleAddUser} />
-      
-      
-{/* <AddUserDrawer/> */}
-   
-        {/* <UserList searchQuery={query} /> */}
-        {/* <UserList searchQuery={query}/> */}
- <UserList searchQuery={query}/>      
-        
-        
-           
-      
+
+      <UserList searchQuery={query} />
     </div>
   );
 }
